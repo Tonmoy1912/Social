1127d1c2a7207dc7ba8eb1726489ef52341f278f
Tonmoy <tonmoybiswas19122002@gmail.com>
2023-05-10 10:04:07 +0530

    sign in and sign out added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,26 @@ const User2=require("../models/user");
 const User=User2.User;
 
 module.exports.profile=function(req,res){
-    // res.send("<h1>User Profile</h1>");
-    res.render("user",{title:"User Profile"});
-    return ;
+    if(req.cookies.user_id==undefined){
+        //console.log(req.cookies.user_id);
+        res.redirect("/users/sign-in");
+        return;
+    }
+    const func=async function(){
+        var arr=await User.find({_id:req.cookies.user_id});
+        if(arr.length==0){
+            res.redirect("/users/sign-in");
+            return ;
+        }
+        var user=arr[0];
+        // console.log(user);
+        res.render("profile",{title:"User Profile",user:user});
+    };
+
+    func();
+
+    // res.render("profile",{title:"User Profile"});
+    // return ;
 }
 
 module.exports.home=function(req,res){
@@ -34,15 +51,20 @@ module.exports.create=function(req,res){
         return res.redirect("back");
     }
     var func=async function(){
-        var obj=await User.find({email:body.email});
-        if(obj.length!=0){
-            res.redirect("back");
+        try{
+            var obj=await User.find({email:body.email});
+            if(obj.length!=0){
+                res.redirect("back");
+            }
+            else{
+                var doc=new User(body);
+                // console.log(doc);
+                await User.insertMany([doc]);
+                res.redirect("/users/sign-in");
+            }
         }
-        else{
-            var doc=new User(body);
-            // console.log(doc);
-            await User.insertMany([doc]);
-            res.redirect("/users/sign-in");
+        catch(e){
+            console.log(e);
         }
     };
     func();
@@ -50,5 +72,30 @@ module.exports.create=function(req,res){
 
 //Sign in and create a session for the user
 module.exports.createSession=function(req,res){
-    //todo later
+    const func=async function(){
+        try{
+            var arr=await User.find({email:req.body.email});
+            if(arr.length==0){
+                return res.redirect("back");
+            }
+            var doc=arr[0];
+            if(doc.password==req.body.password){
+                res.cookie("user_id",doc.id);
+                return res.redirect("/users/profile");
+            }
+            else{
+                return res.redirect("back");
+            }
+        }
+        catch(e){
+            console.log(e);
+        }
+    };
+    func();
 }
+
+//sign out and delete a session
+module.exports.deleteSession=function(req,res){
+    res.clearCookie("user_id");
+    res.redirect("/users/sign-in");
+};
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@ router.get("/",userController.home);
 router.get("/posts",userController.post);
 router.get("/sign-in",userController.signIn);
 router.get("/sign-up",userController.signUp);
+router.get("/delete-session",userController.deleteSession);
 router.post("/create",userController.create);
+router.post("/create-session",userController.createSession);
 
 module.exports=router;
