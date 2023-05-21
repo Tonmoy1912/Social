48292f0994126663c18c8deb433a3e4e818b76a7
Tonmoy Biswas <tonmoybiswas19122002@gamil.com>
2023-05-21 19:08:04 +0530

    post schema and form created

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app=express();
 const port=8000;
 
 const db=require("./config/mongoose");
-const User=require("./models/user");
+const User=require("./models/user").User;
 
 app.set('view engine','ejs');
 app.set('views',"./views");
